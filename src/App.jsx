bad412f6f94f61ceeb2a7ee7b674935affe2f17b
윤진 <151013932+maruru301@users.noreply.github.com>
2025-10-03 20:25:07 +0900

    feat: TMDB Top Rated Movies API 연동 및 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import { fetchTopRatedMovies } from './api/tmdb';
+
 function App() {
-    return <div>Hello</div>;
+    const [movies, setMovies] = useState([]); // 영화 리스트 상태
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const movies = await fetchTopRatedMovies();
+                setMovies(movies);
+            } catch (err) {
+                console.error('영화 데이터 불러오기 실패', err);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Reelyoon</h1>
+            <ul>
+                {movies.map((movie) => (
+                    <li key={movie.id}>{movie.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/api/tmdb.js
@@ -0,0 +1,23 @@
+const API_TOKEN = import.meta.env.VITE_TMDB_API_TOKEN;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_TOKEN}`,
+    },
+};
+
+// Top Rated Movies
+export const fetchTopRatedMovies = async (page = 1) => {
+    const url = `${BASE_URL}/movie/top_rated?language=ko&page=${page}`;
+    try {
+        const res = await fetch(url, options);
+        const data = await res.json();
+
+        return data.results;
+    } catch (err) {
+        console.error('Top Rated Movies API 호출 실패:', err);
+    }
+};
