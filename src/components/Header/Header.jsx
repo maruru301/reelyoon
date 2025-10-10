186d0748f5a957f093c037900b598b5747822640
윤진 <151013932+maruru301@users.noreply.github.com>
2025-10-10 21:19:39 +0900

    feat: header 햄버거 메뉴 구현 (useState로 메뉴 토글)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,31 @@
 import './Header.css';
 
+import hamburger from '../../assets/hamburger.svg';
+import { useState } from 'react';
+
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <header className="header">
             <div className="header-bar">
-                <nav className="nav">hamberger</nav>
+                <nav className="nav">
+                    <button className="menu-btn" onClick={() => setMenuOpen((isOpen) => !isOpen)}>
+                        <img src={hamburger} alt="메뉴" />
+                    </button>
+
+                    <ul className={`menu-list ${menuOpen ? 'open' : ''}`}>
+                        <li>
+                            <a href="/movie">영화</a>
+                        </li>
+                        <li>
+                            <a href="/tv">TV 프로그램</a>
+                        </li>
+                        <li>
+                            <a href="/person">인물</a>
+                        </li>
+                    </ul>
+                </nav>
 
                 <a className="logo gradient-text" href="/">
                     <div>ReelYoon</div>
