44cf38bab33d3565b225468435ce6c2de219ab65
윤진 <151013932+maruru301@users.noreply.github.com>
2025-10-12 15:36:09 +0900

    feat: ContentCard 컴포넌트 UI 및 hover 효과 구현

--- src/components/Main/ContentCard.jsx
+++ src/components/Main/ContentCard.jsx
@@ -1,10 +1,18 @@
+import Star from '../../assets/star.svg';
+
 const ContentCard = ({ content }) => {
     const posterUrl = `https://image.tmdb.org/t/p/original${content.poster_path}`;
 
     return (
         <div className="content-card">
-            <h3>{content.title}</h3>
             <img src={posterUrl} alt={content.title} />
+
+            <h3 className="fade-up">{`${content.title} (${content.release_date.slice(0, 4)})`}</h3>
+
+            <div className="vote-average fade-up">
+                <img src={Star} alt="별 아이콘" />
+                <span>{content.vote_average.toFixed(1)}</span>
+            </div>
         </div>
     );
 };
