5b3ead4b95a1066a4c08669447a14c779c69d704
윤진 <151013932+maruru301@users.noreply.github.com>
2025-10-12 19:33:52 +0900

    feat: Main에서 Popular 영화 데이터 추가 및 ContentList로 렌더링

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,15 @@
 import './Main.css';
 
-import { fetchTopRatedMovies, fetchTrendingMovies } from '../../api/tmdb';
+import { fetchPopularMovies, fetchTopRatedMovies, fetchTrendingMovies } from '../../api/tmdb';
 import { useEffect, useState } from 'react';
 
 import ContentList from './ContentList';
 
 const Main = () => {
-    const [topRated, setTopRated] = useState([]); // Top Rated 영화 리스트
     const [trending, setTrending] = useState([]);
+    const [topRated, setTopRated] = useState([]); // Top Rated 영화 리스트
     const [timeWindow, setTimeWindow] = useState('day');
+    const [popular, setPopular] = useState([]);
 
     // Trending (Daily / Weekly)
     useEffect(() => {
@@ -36,6 +37,19 @@ const Main = () => {
         fetchData();
     }, []);
 
+    // Popular
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const popularData = await fetchPopularMovies();
+                setPopular(popularData);
+            } catch (err) {
+                console.error('Top Rated 영화 데이터 불러오기 실패', err);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <main className="main">
             <ContentList
@@ -46,6 +60,7 @@ const Main = () => {
                 setTimeWindow={setTimeWindow}
             />
             <ContentList title="Top Rated Movies" contents={topRated} />
+            <ContentList title="Popular" contents={popular} />
         </main>
     );
 };
